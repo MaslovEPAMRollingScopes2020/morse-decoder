103c189832f763e48c4071a8ee627d69d7159c00
MaslovEPAMRollingScopes2020 <epamrollingscopes@gmail.com>
2020-10-24 17:57:31 +0300

    init: start task Morse code decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let i = 0;
+  let finalStr = [];
+  let morseKeys = Object.getOwnPropertyNames(MORSE_TABLE);
+  let newMorseTable = {};
+  morseKeys.forEach((item) => {
+    let i = 0;
+    let tempStr = [];
+    
+    for(i = 0; i < item.length; i++){
+      tempStr.push(item[i] === '.' ? '10' : '11');
+    }
+
+    for(i = 5 - i; i > 0; i--){
+      tempStr.unshift('00');
+    }
+
+    newMorseTable[tempStr.join('')] = MORSE_TABLE[item];
+  });
+
+  newMorseTable['**********'] = ' ';
+
+  for(i = 0; i < expr.length; i += 10){
+    finalStr.push(newMorseTable[expr.slice(i, i + 10)]);    
+  }
+  return finalStr.join('');
 }
 
 module.exports = {
